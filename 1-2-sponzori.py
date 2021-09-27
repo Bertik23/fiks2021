01ab5536778f1273e91d3d07677947d14fb796b4
Bertik23 <39457484+Bertik23@users.noreply.github.com>
2021-09-27 18:22:16 +0200

    maxBPM

--- 1-2-sponzori.py
+++ 1-2-sponzori.py
@@ -3,28 +3,63 @@ class Animal:
         self.id = id_
         self.name = name
         self.potencialSponzors = []
-        self.sponzor = None
 
     def __str__(self):
         return (
             f"{self.name} - {self.id} - "
-            f"{self.potencialSponzors} - {self.sponzor}"
+            f"{self.potencialSponzors}"
         )
 
     def __repr__(self):
         return str(self)
 
-    def sponzorStr(self):
-        return f"{self.name} {self.sponzor}"
+
+class Graph:
+    def __init__(self, animalDict: dict, sponzorDict: dict):
+        self.animalDict = animalDict
+        self.animals: list[Animal] = list(animalDict.values())
+        self.sponzorDict = sponzorDict
+        self.sponzors = list(sponzorDict.keys())
+        self.nodes = self.animals + self.sponzors
+        self.graph = {}
+        for a in self.animals:
+            self.graph[a.id] = a.potencialSponzors
+        for s in self.sponzors:
+            self.graph[s] = self.sponzorDict[s]
+
+    def canMatch(
+        self,
+        animal: Animal,
+        matched: dict[str, Animal],
+        seen: dict[str, bool]
+    ):
+        for sponzor in self.sponzors:
+            if (
+                animal.id in self.graph[sponzor]
+                and not seen.get(sponzor, False)
+            ):
+                seen[sponzor] = True
+                if (
+                    matched.get(sponzor, None) is None
+                    or self.canMatch(matched[sponzor], matched, seen)
+                ):
+                    matched[sponzor] = animal
+                    return True
+        return False
+
+    def findMatching(self):
+        match = {}
+        for animal in self.animals:
+            seen = {}
+            self.canMatch(animal, match, seen)
+        return match
 
 
 with open("1-2-sponzori.vstup") as f:
     inputString = f.read()
 
 inputString = inputString.split("\n")
-print(inputString)
 line = inputString[0].split(" ")
-print(line)
 inputString = inputString[1:]
 animalsNum = int(line[0])
 sponzorsNum = int(line[1])
@@ -46,38 +81,15 @@ for i in range(sponzorsNum):
 for sponzor in sponzors:
     for i in sponzors[sponzor]:
         animals[i].potencialSponzors.append(sponzor)
-print(animals)
-sponzoredAnimals = []
-while len(animals):
-    a = animals[min(animals, key=lambda x: len(animals[x].potencialSponzors))]
-    print(a)
-    if not len(a.potencialSponzors):
-        animals.pop(a.id)
-        continue
-    a.sponzor = min(
-        a.potencialSponzors,
-        key=lambda x: len(
-            [
-                animal for animal in animals.values()
-                if x in animal.potencialSponzors
-            ]
-        )
-    )
-    print(sponzoredAnimals)
-    sponzors.pop(a.sponzor)
-    for animal in animals:
-        try:
-            print(animals[animal].potencialSponzors, a.sponzor)
-            animals[animal].potencialSponzors.remove(a.sponzor)
-        except ValueError:
-            pass
-    sponzoredAnimals.append(a)
-    animals.pop(a.id)
 
-print(sponzoredAnimals)
-if len(sponzoredAnimals) == animalsNum:
+print(animals)
+print(sponzors)
+g = Graph(animals, sponzors)
+
+matching = g.findMatching()
+if len(matching) == animalsNum:
     print("Ano")
 else:
     print("Ne")
-for a in sorted([i.sponzorStr() for i in sponzoredAnimals]):
-    print(a)
+for sponzor in sorted(matching, key=lambda x: matching[x].name):
+    print(matching[sponzor].name, sponzor)
