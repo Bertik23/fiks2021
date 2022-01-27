66965819033f2ec7c1175cac8e9a23b968670965
Bertik23 <bertikxxiii@gmail.com>
2022-01-27 22:03:08 +0100

    prostě to nefunguje

--- rust/k4u4cizokrajny_prikazovaci_urad/src/main.rs
+++ rust/k4u4cizokrajny_prikazovaci_urad/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 
 fn zfill(in_string: String, length: usize) -> String{
@@ -49,11 +49,12 @@ fn parse_instruction(instruction_argument: u32) -> Option<(u8, String, u32)>{
 struct Process{
     pc: usize,
     stack: Vec<u32>,
-    alive: bool
+    alive: bool,
+    waiting_for_tp: bool
 }
 
 impl Process{
-    fn new(pc: u8, memory: Vec<u32>, main_memory: &mut [u32]) -> Self{
+    fn new(pc: u8, memory: Vec<u32>, main_memory: &mut [u32], instructions_hashset: &mut HashSet<String>) -> Self{
         //let mem: [u32; 256] = [0; 256];
         let mut load_pc = pc as usize;
         for instruction in memory.into_iter(){
@@ -61,14 +62,18 @@ impl Process{
                 main_memory[load_pc] = instruction;
             }
             let parsed = parse_instruction(instruction).unwrap_or_default();
+            instructions_hashset.insert(parsed.1);
             // eprintln!("{} {}", parsed.1, parsed.2);
             load_pc += 1;
             load_pc = load_pc % 256;
         }
-        Self{pc: pc as usize, stack: vec![], alive: true}
+        Self{pc: pc as usize, stack: vec![], alive: true, waiting_for_tp: false}
     }
 
     fn cycle(&mut self, main_memory: &mut [u32]) -> bool{
+        if self.waiting_for_tp{
+            return true
+        }
         let instruction_argument = main_memory[self.pc];
         // let bin_inst = zfill(format!("{:b}", instruction_argument), 32usize);
         // let instruction = u8::from_str_radix(&String::from_utf8_lossy(&bin_inst.as_bytes()[24..32]), 2).unwrap();
@@ -198,7 +203,8 @@ impl Process{
         } else if instruction_str == "BOMB"{
             main_memory[self.pc] -= 1;
         } else if instruction_str == "TLPORT"{
-            todo!()
+            self.waiting_for_tp = true;
+            return true
         } else if instruction_str == "JNTAR"{
             for i in 1..=3{
                 let a = self.pc.wrapping_add(2usize.pow(i));
@@ -226,26 +232,50 @@ fn main() {
     let mut line = String::new();
     let mut _b1 = std::io::stdin().read_line(&mut line);
     let num_of_problems: u16 = line.trim().parse().unwrap();
-    for _problem_n in 0..num_of_problems{
+    let mut instructions_hashset: HashSet<String> = HashSet::new();
+    for problem_n in 0..num_of_problems{
+        if problem_n == 382{
+            eprintln!("{:?}", instructions_hashset)
+        }
+        if problem_n == 490{
+            eprintln!("{:?}", instructions_hashset)
+        }
         line.clear();
         _b1 = std::io::stdin().read_line(&mut line);
         // eprintln!("Line: {}\nBytes: {:?}", line, line.as_bytes());
-        let num_of_processes: u8 = line.trim().parse().unwrap();
+        let num_of_processes: u16 = line.trim().parse().unwrap();
         let mut processes = vec![];
         let mut main_memory: [u32; 256] = [0u32; 256];
         for _process_n in 0..num_of_processes{
             line.clear();
             _b1 = std::io::stdin().read_line(&mut line);
             let line_vec = line.trim().split(" ").map(|p| p.parse::<u32>().unwrap()).collect::<Vec<u32>>();
-            processes.push(Process::new(line_vec[0] as u8, line_vec[2..].to_vec(), &mut main_memory));
+            processes.push(Process::new(line_vec[0] as u8, line_vec[2..].to_vec(), &mut main_memory, &mut instructions_hashset));
         }
+        let mut waiting_for_tp = vec![];
         for _c in 0..5000{
             for p in 0..processes.len(){
+                waiting_for_tp.clear();
                 if !processes[p].alive{
                     continue
                 }
                 if !processes[p].cycle(&mut main_memory){
                     processes[p].alive = false;
+                }
+                if processes[p].waiting_for_tp{
+                    waiting_for_tp.push(p);
+                }
+                if waiting_for_tp.len() >= 2{
+                    let first = processes[waiting_for_tp[0]].pc;
+                    for (i, pj) in waiting_for_tp.iter().enumerate(){
+                        if i+1 < waiting_for_tp.len(){
+                            processes[*pj].pc = processes[waiting_for_tp[i+1]].pc + 1;
+                        } else {
+                            processes[*pj].pc = first + 1;
+                        }
+                        processes[*pj].pc = processes[*pj].pc % 256;
+                        processes[*pj].waiting_for_tp = false;
+                    }
                 }
             }
         }
